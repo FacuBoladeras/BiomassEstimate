b92e3c7b98ef6b4c081f211dfb06a16da709bb98
Facundo <facub5335@gmail.com>
2024-08-12 18:27:31 -0300

    texture

--- Scripts/resample100.py
+++ Scripts/resample100.py
@@ -36,8 +36,8 @@ def resample_raster(input_tiff, output_tiff, scale_factor):
     print(f"Nuevo TIFF remuestreado guardado en: {output_tiff}")
 
 # Rutas de los archivos de entrada y salida
-input_tiff = r"C:\Users\Facu\Downloads\Malawi_2020-prueba (1).tif"
-output_tiff = "malawi-resample.tif"
+input_tiff = r"C:\Users\Facu\Downloads\Malawi_2010-L-P (1).tif"
+output_tiff = "malawi-resample-2010.tif"
 
 # Factor de escala para remuestrear a 100x100 metros desde 30x30 metros
 scale_factor = 100 / 30
